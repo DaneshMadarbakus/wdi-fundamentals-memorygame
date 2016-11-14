9cf128f6e9404c113354cb3125517f3a6b166d5f
Danesh Madarbakus <DaneshMadarbakus@daneshs-mbp.home>
2016-11-14 20:06:15 +0000

    introduced alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,23 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+}
+else if (cardThree === cardFour) {
+	alert('You found a match!');
+}
+else if (cardThree === cardOne) {
+	alert('Sorry, try again.');
+}
+else if (cardThree === cardTwo) {
+	alert('Sorry, try again.');
+}
+else if (cardFour === cardOne) {
+	alert('Sorry, try again.');
+}
+else if (cardFour === cardTwo) {
+	alert('Sorry, try again.');
+}
