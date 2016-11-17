1852485a8acfe6ff207e230b23abd9c80b55be3d
Danesh Madarbakus <DaneshMadarbakus@daneshs-mbp.home>
2016-11-17 02:27:29 +0000

    completed memory game

--- starter-code/main.js
+++ starter-code/main.js
@@ -25,7 +25,7 @@ else if (cardFour === cardTwo) {
 	alert('Sorry, try again.');
 } */
 
-var gameBoard = document.getElementById('game-board');
+/*var gameBoard = document.getElementById('game-board');
 var createCards = function(y) {
 	for (var x = 0; x < y; x++) {
 	var newCard = document.createElement('div');
@@ -33,4 +33,52 @@ var createCards = function(y) {
 	gameBoard.appendChild(newCard); 
 	}
 }
-createCards(4) 
+createCards(4) */
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+var createBoard = function createBoard() {
+	for (var i = 0; i < cards.length; i++){
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+	board.appendChild(cardElement);
+	board.appendChild(cardElement);
+	}
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
+	}
+	else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+var isMatch = function(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+	  alert("Sorry, try again.");
+	}
+}
+
+createBoard();
+
+
+
+
+
+
+
+
